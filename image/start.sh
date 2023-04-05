b3a2b1a7b04fcdf88b9d10da5096c303236c5caa
A.Lepe <lepe@support.ne.jp>
2023-04-05 14:07:09 +0900

    modified start.sh

--- image/start.sh
+++ image/start.sh
@@ -9,6 +9,9 @@ fi
 fpm_config=/etc/php/php-fpm.d/www.conf
 sed -i "s/PHP_MIN_WORKERS/$PHP_MIN_WORKERS/g" "$fpm_config"
 sed -i "s/PHP_MAX_WORKERS/$PHP_MAX_WORKERS/g" "$fpm_config"
+if [[ $SITE_CHARSET != "" ]]; then
+	sed -i "s/UTF-8/$SITE_CHARSET/g" /etc/php5/php.ini
+fi
 echo "Starting PHP-FPM...."
 php-fpm -D
 echo "Starting lighttpd...."
