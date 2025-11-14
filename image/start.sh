fd098ae59d1327859de8b8360f3acf50f0ee1a7c
A.Lepe <lepe@support.ne.jp>
2025-11-14 15:02:53 +0900

    Fixed custom config not being used

--- image/start.sh
+++ image/start.sh
@@ -9,6 +9,7 @@ fi
 fpm_config=/etc/php/php-fpm.d/www.conf
 sed -i "s/PHP_MIN_WORKERS/$PHP_MIN_WORKERS/g" "$fpm_config"
 sed -i "s/PHP_MAX_WORKERS/$PHP_MAX_WORKERS/g" "$fpm_config"
+ln -s $fpm_config /etc/php5/fpm.d/
 if [[ $SITE_CHARSET != "" ]]; then
 	sed -i "s/UTF-8/$SITE_CHARSET/g" /etc/php5/php.ini
 fi
