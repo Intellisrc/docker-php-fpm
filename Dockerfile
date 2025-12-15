da69b6c5601579984a9522a2072812989bb3d6ce
A.Lepe <lepe@support.ne.jp>
2025-12-15 13:36:13 +0900

    Added fileinfo & PHP variables

--- Dockerfile
+++ Dockerfile
@@ -25,7 +25,8 @@ RUN apk add --update --no-cache \
 	curl lighttpd \
 	php$PHP_VER-fpm php$PHP_VER-ctype php$PHP_VER-common php$PHP_VER-intl \
 	php$PHP_VER-curl php$PHP_VER-gd php$PHP_VER-json php$PHP_VER-mysqli php$PHP_VER-xml \
-	php$PHP_VER-zip php$PHP_VER-dom php$PHP_VER-iconv php$PHP_VER-opcache php$PHP_VER-exif && \
+	php$PHP_VER-zip php$PHP_VER-dom php$PHP_VER-iconv php$PHP_VER-opcache php$PHP_VER-exif \
+	php$PHP_VER-fileinfo && \
 	rm -rf /var/cache/apk/*
 
 COPY image/lighttpd.conf /etc/lighttpd/
--- image/start.sh
+++ image/start.sh
@@ -10,7 +10,22 @@ fpm_config=/etc/php/php-fpm.d/www.conf
 sed -i "s/PHP_MIN_WORKERS/$PHP_MIN_WORKERS/g" "$fpm_config"
 sed -i "s/PHP_MAX_WORKERS/$PHP_MAX_WORKERS/g" "$fpm_config"
 if [[ $SITE_CHARSET != "" ]]; then
-	sed -i "s/UTF-8/$SITE_CHARSET/g" /etc/php5/php.ini
+	sed -i "s/UTF-8/$SITE_CHARSET/g" /etc/php/php.ini
+fi
+if [[ $PHP_MAX_UPLOAD != "" ]]; then
+	sed -i "s/20M/$PHP_MAX_UPLOAD/g" /etc/php/php.ini
+fi
+if [[ $PHP_MEMORY_LIMIT != "" ]]; then
+	sed -i "s/128M/$PHP_MEMORY_LIMIT/g" /etc/php/php.ini
+fi
+if [[ $PHP_INPUT_TIME != "" ]]; then
+	sed -i "s/= 400/= $PHP_INPUT_TIME/g" /etc/php/php.ini
+fi
+if [[ $PHP_EXECUTION_TIME != "" ]]; then
+	sed -i "s/= 600/$PHP_EXECUTION_TIME/g" /etc/php/php.ini
+fi
+if [[ $PHP_FILE_UPLOADS != "" ]]; then
+	sed -i "s/max_file_uploads = 20/max_file_uploads = $PHP_FILE_UPLOADS/g" /etc/php/php.ini
 fi
 echo "Starting PHP-FPM...."
 php-fpm -D
